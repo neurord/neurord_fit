0da22f8dec46363b831cb293e2c161824999d21d
Avrama Blackwell <avrama@gmu.edu>
2018-05-11 17:41:26 -0400

    CSV_conc_set class for set of experiments

--- loadconc.py
+++ loadconc.py
@@ -3,6 +3,10 @@
 
 from __future__ import print_function, division
 import numpy as np
+from ajustador import xml
+import glob    
+import os
+import operator
 
 class trace(object):
     def __init__(self, molname, x, y):
@@ -16,13 +20,15 @@ class CSV_conc(object):
     Each CSV file contains data for one or more molecules:
       Time time_units, mol_name1 (nM), [mol_name2]
       read time_units (sec,msec,min allowed) and convert to msec
-    need to update (add super object?) to allow multiple files, differing in stim protocol
     """
-    def __init__(self, fname,features=[]):
+    def __init__(self, fname,rootname,features=[]):
         import pandas as pd
-        self.name=fname
-        self.features=[]
-        csv = pd.read_csv(self.name, index_col=0)
+        model_num=xml.modelname_to_param(fname,rootname)
+        self.name=os.path.basename(fname)[0:os.path.basename(fname).rfind('.')]
+        self.injection=model_num
+        self.features=features
+        
+        csv = pd.read_csv(fname, index_col=0)
         x_head=csv.index.name.split()
         if len(x_head)>1:
             time_units=x_head[-1]
@@ -36,5 +42,32 @@ class CSV_conc(object):
         else:
             time_factor=1
         x = csv.index.values*time_factor #time values
+        
         self.waves = {col.split()[0]:trace(col.split()[0], x, csv[col].values) for col in csv.columns}
+
+class CSV_conc_set(object):
+    #set of files, each one a CSV_conc object, differing in stim protocol
+    def __init__(self,rootname,features=[]):
+        self.features=features
+        if os.path.isdir(rootname): #if directory, look for all csv files
+            dirname = rootname
+            filenames=glob.glob(rootname+'/*.csv')
+            self.name=rootname
+        else:
+            if rootname.endswith('.csv'):
+                #case with single filename specified
+                filenames=[rootname]
+            else:
+                #case with a set of filenames specified, with common "prefix" + variable "suffix"
+                filenames=glob.glob(rootname+'*.csv')
+            dirname = os.path.dirname(rootname)
+            self.name=os.path.basename(rootname)
+        print('CSV_conc_set:',self.name, 'rootname', rootname,'dir',dirname,'files',filenames)
+        if len(filenames)==0:
+            print('**************** CSV_conc_set: NO FILES FOUND **************************')
+        
+        csv_list=[CSV_conc(fn,rootname) for fn in filenames]
+        csv_list.sort(key=operator.attrgetter('injection'))
+        self.data=csv_list
+        #allexp={series.name.split('/')[-1].split('.')[0]:series for series in csv_list}
 
