2d0b85c07fe314009b7a25b13e5daad06d0b86c3
Avrama Blackwell <avrama@gmu.edu>
2018-05-18 12:38:37 -0400

    To Do list moved from python script to README

--- neurord_fit_CamKII.py
+++ neurord_fit_CamKII.py
@@ -1,30 +1,22 @@
-#How to use :doc:`ajustador` to fit a NeuroRD model
+#How to use :doc:`ajustador` to fit a NeuroRD model of CamKII activation
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-'''This demonstration fits a model of CamKII to a previous models results
-   simulate CamCa4 pulses at different frequencies, possibly include models with 6 s of constant stimulation at different CamCa4 concentrations.  May need to inject Ca and CaBuf instead of CamBuf to get better CamCa4 levels. 
-Next Steps:
-4. Add variation of IC parameters (important)
-
-5. Specify parameters to vary in a separate file (less important)
-     possibly with xml specifications similar to the model instead of xpath - see neurord3_params_sims.py and neurord_2params_sims.py
-
-6. add features to fitness function, e.g. peak amplitude and width 
-    need to specify stimulus onset time, as with injection current. 
-    Attach fitness values to fit object?
-'''
 
 import ajustador as aju
 import numpy as np
 from ajustador import drawing,loadconc,neurord_fit
 from ajustador.helpers import converge
+import os
 
-#model is the xml file that contains the neuroRD model to simulate and adjust parameters
-dirname='camkii/'  #location of data and model files.
+dirname='camkii/'  #where data and model file are stored.  Can be different than current directory. Multiple datafiles allowed
 #Set of model files that have first part of file name in common.  All included files must be in same directory.
-model_set=dirname+'Model-CKnew-Cahz'
-exp_set=dirname+'Model-CKold-Cahz' #set of data files corresponding to model files; files may contain several molecules
+model_set='Model-CKnew-Cahz'
+exp_set='Model-CKold_1.5ux2-Cahz' #set of data files corresponding to model files; files may contain several molecules
 mol=['CKpCamCa4','CKCamCa4'] #which molecule(s) to match in optimization
 tmpdir='/tmp/'+dirname 
+os.chdir(dirname)
+
+#this command indicates that experiments are from a previous simulation
+exp = aju.xml.NeurordResult(exp_set)
 
 # number of iterations, use 1 for testing
 # default popsize=8, use 3 for testing
@@ -42,7 +34,6 @@ params = aju.optimize.ParamSet(P('CK2_fwd_rate', 0, min=0, max=1e-6, xpath='//Re
                                P('CK2_CKp2_fwd_rate', 0, min=0, max=1e-9, xpath='//Reaction[@id="CK2_CKpCam_pow2"]/forwardRate'))
 
 ###################### END CUSTOMIZATION #######################################
-exp = aju.xml.NeurordResult(exp_set)
 
 fitness = neurord_fit.specie_concentration_fitness(species_list=mol)
 
